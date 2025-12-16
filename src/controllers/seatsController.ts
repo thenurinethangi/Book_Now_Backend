3736e8e33e5483597831fbc4d8a024a71caa08fc
thenurinethangi <thenurinathangi@gmail.com>
2025-12-17 00:00:17 +0530

    develop seat lock

--- src/controllers/seatsController.ts
+++ src/controllers/seatsController.ts
@@ -1,9 +1,13 @@
 import { Response } from "express";
 import { AuthRequest } from "../middlewares/authenticate";
 import { redis } from "../config/redis";
+import { Showtime } from "../models/Showtime";
 
 export const lockSeats = async (req: AuthRequest, res: Response) => {
-    const { showId, seats, userId } = req.body;
+
+    const { showId, seats } = req.body;
+
+    const userId = req.sub;
 
     for (const seat of seats) {
         const key = `seat_lock:${showId}:${seat}`;
@@ -14,13 +18,98 @@ export const lockSeats = async (req: AuthRequest, res: Response) => {
             { nx: true, ex: 300 }
         );
 
-        if (!locked) {
-            return res.status(409).json({
-                message: `Seat ${seat} is already locked`,
-                data: null
-            });
-        }
+        // if (!locked) {
+        //     return res.status(409).json({
+        //         message: `Seat ${seat} is already locked`,
+        //         data: null
+        //     });
+        // }
     }
 
     res.status(200).json({ message: "Seats locked for 5 minutes", data: null });
 };
+
+
+export const checkIsLock = async (req: AuthRequest, res: Response) => {
+
+    const { showId, seatNo } = req.body;
+    const currentUserId = req.sub;
+
+    const key = `seat_lock:${showId}:${seatNo}`;
+
+    const lockData: any = await redis.get(key);
+
+    if (!lockData) {
+        return res.status(200).json({
+            locked: false,
+            data: null,
+        });
+    }
+
+    res.status(200).json({
+        locked: true,
+        lockedByMe: lockData.userId === currentUserId,
+        data: lockData,
+    });
+};
+
+
+export const checkLockedSeats = async (req: AuthRequest, res: Response) => {
+
+    const showId = req.params.showtimeId;
+    const currentUserId = req.sub;
+
+    console.log(showId);
+
+    if (!showId) {
+        res.status(400).json({ message: "Showtime ID not provided!", data: null });
+        return;
+    }
+
+    try {
+        const showtime = await Showtime.findOne({ _id: showId });
+        console.log(showtime);
+
+        if (!showtime) {
+            res.status(400).json({ message: "Showtime not found!", data: null });
+            return;
+        }
+
+        const seatPromises: Promise<any>[] = [];
+
+        for (let i = 0; i < showtime.seats.length; i++) {
+            const row = showtime.seats[i];
+            for (let j = 0; j < row.length; j++) {
+                const seat: any = row[j];
+                if (!seat) continue;
+
+                const key = `seat_lock:${showId}:${seat.id}`;
+                console.log(key);
+
+                // Push the Redis get promise to array
+                seatPromises.push(
+                    redis.get(key).then((lockData: any) => {
+                        if (!lockData) {
+                            return { locked: false, data: null };
+                        } else {
+                            return { locked: true, lockedByMe: lockData.userId === currentUserId, data: lockData };
+                        }
+                    })
+                );
+            }
+        }
+
+        // Wait for all Redis queries in parallel
+        const arr = await Promise.all(seatPromises);
+        console.log('--------------------');
+        console.log(arr);
+
+        res.status(200).json({ message: 'Successfully load seat locking result!', data: arr });
+        return;
+    } catch (e) {
+        res.status(500).json({ message: "Failed to load seat locking result!", data: null });
+        return;
+    }
+};
+
+
--- src/routes/seatsRoute.ts
+++ src/routes/seatsRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { lockSeats } from '../controllers/seatsController';
+import { checkIsLock, checkLockedSeats, lockSeats } from '../controllers/seatsController';
 import { authenticate } from '../middlewares/authenticate';
 import { authorize } from '../middlewares/authorize';
 import { Role } from '../models/User';
@@ -7,5 +7,9 @@ import { Role } from '../models/User';
 const router = express.Router();
 
 router.post('/lock',authenticate,authorize([Role.USER]),lockSeats);
+
+router.post('/check/islock',authenticate,authorize([Role.USER]),checkIsLock);
+
+router.get('/check/all/islock/:showtimeId',authenticate,authorize([Role.USER]),checkLockedSeats);
 
 export default router;
